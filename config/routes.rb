ce7cbb0b29b664377bc0298f86a7680279e96302
AlbertoJTD <albertojtd@outlook.com>
2023-01-02 18:04:29 -0600

    Tweet model & controller added

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'tweets#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tweets, except: [:edit, :update]
 end
