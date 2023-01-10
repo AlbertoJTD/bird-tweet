114c59078ab28cd407678a95445d175a5642f15e
AlbertoJTD <albertojtd@outlook.com>
2023-01-09 18:37:28 -0600

    username field added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -20,6 +21,7 @@
 #
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
   has_person_name
@@ -30,4 +32,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :tweets
+
+  validates_uniqueness_of :username
 end
